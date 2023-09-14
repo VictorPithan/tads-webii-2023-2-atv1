8a2babc9dc19696d268bfc65daba48ef9f07ce99
VictorPithan <victorpithan@gmail.com>
2023-09-14 14:54:19 -0300

    feat:add lib ejs and path

--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
+const path = require("path");
 const express = require("express"); 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.render('Funciona');
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// FORM DATA
+app.use(express.urlencoded({ extended: false }));
+// API JSON
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/teste', (req, res) => {
+    res.send('Funciona');
 });
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
